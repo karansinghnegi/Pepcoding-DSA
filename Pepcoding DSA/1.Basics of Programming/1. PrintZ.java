f736a2064ff88f8c1cea012c1138266e41cd862c
karan <lost.cause.ksn@gmail.com>
2022-05-12 07:34:27 +0530

    DSA Question

---
+++ Pepcoding DSA/1.Basics of Programming/1. PrintZ.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class PrintZ {
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.print("*****\n");
+        System.out.print("   *\n");
+        System.out.print("  *\n");
+        System.out.print(" *\n");
+        System.out.print("*****");
+
+    }
+}
